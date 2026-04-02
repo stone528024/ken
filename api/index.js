4b866b40d0f5513e27adbba9c6eba492de1b7f2b
幼儿园扛把子 <3802394254@qq.com>
2026-04-02 15:42:34 +0800

    更改域名请求接口

--- api/index.js
+++ api/index.js
@@ -8,8 +8,8 @@ console.log("API KEY 前5位：", GEMINI_API_KEY ? GEMINI_API_KEY.substring(0, 5
 console.log("=========================================");
 
 // 启动服务
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`服务运行在：http://localhost:${PORT}`);
-});
-// module.exports = app;
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`服务运行在：http://localhost:${PORT}`);
+// });
+module.exports = app;
